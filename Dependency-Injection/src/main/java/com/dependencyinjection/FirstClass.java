962c799829b99f6ee1ecf340c0ee885125d7a5fc
Bilal <90500488+berispak@users.noreply.github.com>
2023-03-08 15:11:17 +0300

    Add files via upload

---
+++ Dependency-Injection/src/main/java/com/dependencyinjection/FirstClass.java
@@ -0,0 +1,12 @@
+package com.dependencyinjection;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class FirstClass {
+    public String getName(){
+        return "First Class";
+    }
+
+}
